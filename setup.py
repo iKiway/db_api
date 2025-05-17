ac6c18ffe3fe1d6e8179fd1458fc84094c529f8e
iKiway <105075696+iKiway@users.noreply.github.com>
2025-05-17 15:50:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='db_api',
+    name='db-api',
     version='0.1',
     description='A Python package for interacting with the Deutsche Bahn API',
     author='Kimon Beyer',
