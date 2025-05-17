193db1312b20cf6542e432ed0b170b434fad9c83
iKiway <105075696+iKiway@users.noreply.github.com>
2025-05-17 22:20:10 +0200

    Update Train.py

--- db_api/Train.py
+++ db_api/Train.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 class Train:
     def __init__(self, station_name, arrival_planned, arrival_actual, departure_planned, departure_actual, platform_planned, platform_actual, canceled, train_number, train_type, train_category, train_id, past_destinations, future_destinations):
@@ -19,6 +20,7 @@ class Train:
         self.platform_change = False if platform_actual == None else True
         self.final_destination = self.future_destinations.split('|')[-1] if self.future_destinations else self.station_name
         self.start_station = self.past_destinations.split('|')[0] if self.past_destinations else self.station_name
+        self.delay = None if self.arrival_actual == None else datetime.strptime(self.arrival_actual, "%y%m%d%H%M") - datetime.strptime(self.arrival_planned, "%y%m%d%H%M") if self.arrival_planned != None else None
         
         
     def print_train(self):
